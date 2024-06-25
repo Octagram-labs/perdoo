9d8b102f5a8d6eb626e9485690c56df12c57c32e
shreyas-octagram <167545634+shreyas-octagram@users.noreply.github.com>
2024-06-25 17:59:14 +0530

    Update startfree_form.js

--- startfree_form.js
+++ startfree_form.js
@@ -20,7 +20,7 @@ function handleSubmit(event) {
 
     // Disable the submit button to avoid multiple clicks
     const submitSignupButton = document.querySelector('[data-name="signup-submit"]');
-    submitSignupButton.disabled = true;
+    submitSignupButton.classList.add('disabled');
 
     // Function to get cookie value by name
     function getCookie(name) {
@@ -95,118 +95,113 @@ function handleSubmit(event) {
             }
 
             // If there are no validation errors, proceed with the fetch request
-            if (!emailErrorMessage.style.display || emailErrorMessage.style.display === 'none') {
-                if (!companySizeErrorMessage.style.display || companySizeErrorMessage.style.display === 'none') {
-                    // Change button text to "Please wait..."
-                    submitSignupButton.textContent = 'Please wait...';
+            if ((!emailErrorMessage.style.display || emailErrorMessage.style.display === 'none') && (!companySizeErrorMessage.style.display || companySizeErrorMessage.style.display === 'none')) {
+                // Change button text to "Please wait..."
+                submitSignupButton.textContent = 'Please wait...';
 
-                    // Construct query parameters
-                    const queryParams = new URLSearchParams({
-                        email: email,
-                        country: country,
-                        company_size: companySize
-                    });
+                // Construct query parameters
+                const queryParams = new URLSearchParams({
+                    email: email,
+                    country: country,
+                    company_size: companySize
+                });
 
-                    function sendSignupRequest() {
-                        // Make POST request with query parameters
-                        fetch(`${getBaseUrl()}/hp-signup?${queryParams.toString()}`, {
-                            method: 'POST'
-                        })
-                            .then(response => response.json())
-                            .then(data => {
-                                // Handle response
-                                const signupMessageGreen = document.querySelector('[signupMessage="green"]');
-                                const signupMessageRed = document.querySelector('[signupMessage="red"]');
+                function sendSignupRequest() {
+                    // Make POST request with query parameters
+                    fetch(`${getBaseUrl()}/hp-signup?${queryParams.toString()}`, {
+                        method: 'POST'
+                    })
+                        .then(response => response.json())
+                        .then(data => {
+                            // Handle response
+                            const signupMessageGreen = document.querySelector('[signupMessage="green"]');
+                            const signupMessageRed = document.querySelector('[signupMessage="red"]');
 
-                                if (data.data && data.data.msg) {
-                                    const errorMsg = data.data.msg.toLowerCase(); // Convert response to lowercase
-                                    switch (errorMsg) {
-                                        case 'no_business_domain':
-                                            showEmailError();
-                                            break;
-                                        case 'invalid_email_address':
-                                            showEmailError();
-                                            break;
-                                        case 'company_disabled':
-                                            signupMessageRed.textContent = `The account for ${email} has been disabled. Contact us at support@perdoo.com in case you need help.`;
-                                            signupMessageRed.style.display = 'block';
-                                            break;
-                                        case 'domain_claimed':
-                                            signupMessageRed.textContent = "This domain has been claimed by another organization, or your organization has reached its free user limit. Contact us at support@perdoo.com in case you need help.";
-                                            signupMessageRed.style.display = 'block';
-                                            break;
-                                        case 'email_signup_success':
-                                            signupMessageGreen.textContent = "Please check your email to complete your registration!";
-                                            signupMessageGreen.style.display = 'block';
-                                            document.getElementById("signupFormSubmit").click();
-                                            // Hide the form content
-                                            const formContent = document.querySelector('[form-normal="signup-form"]');
-                                            if (formContent) {
-                                                formContent.style.display = 'none';
-                                            }
-                                            // Display the success message
-                                            const successMessage = document.querySelector('[form-success="signup-form"]');
-                                            if (successMessage) {
-                                                successMessage.style.display = 'block';
-                                            }
-                                            // After successful fetch
-                                            dataLayer.push({
-                                                'event': 'formSubmit',
-                                                'formId': event.target.id,
-                                            });
-                                            break;
-                                        case 'user_exists':
-                                            // Redirect user to the given URL
-                                            window.location.href = data.data.redirect_to;
-                                            break;
-                                        default:
-                                            // Handle other errors
-                                            break;
-                                    }
+                            if (data.data && data.data.msg) {
+                                const errorMsg = data.data.msg.toLowerCase(); // Convert response to lowercase
+                                switch (errorMsg) {
+                                    case 'no_business_domain':
+                                        showEmailError();
+                                        break;
+                                    case 'invalid_email_address':
+                                        showEmailError();
+                                        break;
+                                    case 'company_disabled':
+                                        signupMessageRed.textContent = `The account for ${email} has been disabled. Contact us at support@perdoo.com in case you need help.`;
+                                        signupMessageRed.style.display = 'block';
+                                        break;
+                                    case 'domain_claimed':
+                                        signupMessageRed.textContent = "This domain has been claimed by another organization, or your organization has reached its free user limit. Contact us at support@perdoo.com in case you need help.";
+                                        signupMessageRed.style.display = 'block';
+                                        break;
+                                    case 'email_signup_success':
+                                        signupMessageGreen.textContent = "Please check your email to complete your registration!";
+                                        signupMessageGreen.style.display = 'block';
+                                        document.getElementById("signupFormSubmit").click();
+                                        // Hide the form content
+                                        const formContent = document.querySelector('[form-normal="signup-form"]');
+                                        if (formContent) {
+                                            formContent.style.display = 'none';
+                                        }
+                                        // Display the success message
+                                        const successMessage = document.querySelector('[form-success="signup-form"]');
+                                        if (successMessage) {
+                                            successMessage.style.display = 'block';
+                                        }
+                                        // After successful fetch
+                                        dataLayer.push({
+                                            'event': 'formSubmit',
+                                            'formId': event.target.id,
+                                        });
+                                        break;
+                                    case 'user_exists':
+                                        // Redirect user to the given URL
+                                        window.location.href = data.data.redirect_to;
+                                        break;
+                                    default:
+                                        // Handle other errors
+                                        break;
                                 }
-                            })
-                            .catch(error => {
-                                console.error('Error:', error);
-                            })
-                            .finally(() => {
-                                // Reset button text and enable the button
-                                submitSignupButton.textContent = 'Start for free';
-                                submitSignupButton.disabled = false;
-                            });
-                    }
+                            }
+                        })
+                        .catch(error => {
+                            console.error('Error:', error);
+                        })
+                        .finally(() => {
+                            // Reset button text and enable the button
+                            submitSignupButton.textContent = 'Start for free';
+                            submitSignupButton.classList.remove('disabled');
+                        });
+                }
 
-                    if (companySize === '1-10' || companySize === '11-50') {
-                        sendSignupRequest();
-                    } else if (companySize === '51-500' || companySize === '501-2500' || companySize === '2501+') {
-                        sendSignupRequest();
-                        setTimeout(() => {
-                            // Redirect to Calendly with email parameter
-                            const calendlyUrl = `https://calendly.com/perdoo/onboarding/?email=${encodeURIComponent(email)}&a1=${encodeURIComponent(companySize)}`;
-                            // Open Calendly URL in a new tab
-                            window.open(calendlyUrl, '_blank');
-                        }, 3000); // Delay of 3 seconds
-                    } else {
-                        // Handle other cases if needed
-                    }
+                if (companySize === '1-10' || companySize === '11-50') {
+                    sendSignupRequest();
+                } else if (companySize === '51-500' || companySize === '501-2500' || companySize === '2501+') {
+                    sendSignupRequest();
+                    setTimeout(() => {
+                        // Redirect to Calendly with email parameter
+                        const calendlyUrl = `https://calendly.com/perdoo/onboarding/?email=${encodeURIComponent(email)}&a1=${encodeURIComponent(companySize)}`;
+                        // Open Calendly URL in a new tab
+                        window.open(calendlyUrl, '_blank');
+                    }, 3000); // Delay of 3 seconds
                 } else {
-                    // Enable the button if there are validation errors
-                    submitSignupButton.disabled = false;
+                    // Handle other cases if needed
                 }
             } else {
                 // Enable the button if there are validation errors
-                submitSignupButton.disabled = false;
+                submitSignupButton.classList.remove('disabled');
             }
         })
         .catch(error => {
             console.error('Error fetching blacklisted emails:', error);
             // Enable the button in case of error
-            submitSignupButton.disabled = false;
+            submitSignupButton.classList.remove('disabled');
         });
 }
 
 // Add event listener to the document to handle clicks on elements with data-name="signup-submit"
 document.addEventListener('click', function (event) {
-    if (event.target && event.target.matches('[data-name="signup-submit"]')) {
+    if (event.target && event.target.matches('[data-name="signup-submit"]') && !event.target.classList.contains('disabled')) {
         // Click event happened on a div with data-name="signup-submit"
         console.log('Click event on div with data-name="signup-submit"');
         // Call the form submission function
